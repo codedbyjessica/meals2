930a7f88d87359be9fa42721acf69527a9f70681
Jessica Zhang <yaojiazhang@gmail.com>
2023-07-11 13:06:12 -0400

    favicons and footer copy

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,8 @@ class Footer extends React.Component {
 	render() {
 		return (
             <footer>
-            sometimes <a href="https://codedbyjessica.com/" target="_blank" rel="noreferrer">I</a> just need some measurements, a few instructions and a place to keep them all instead of scrolling through the internet and reading someone's entire family history before you get to the ingredients list just to find out you're missing 1tbsp of crème fraîche
+            sometimes you just need some measurements, a few instructions and a place to keep them all instead of scrolling through the internet and reading someone's entire family history before you get to the ingredients list just to find out you're missing 1tbsp of crème fraîche <br />
+            <a href="https://codedbyjessica.com/" target="_blank" rel="noreferrer">www.codedbyjessica.com</a>
             </footer>
 
 		);
